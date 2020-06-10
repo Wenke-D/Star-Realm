798f5afa9127b3d2435e1d87faa140f0f243aaef
Wenke-D <65447561+Wenke-D@users.noreply.github.com>
2020-06-10 16:57:59 +0200

    Finish function cards

--- Star-Realms/src/model/Store.java
+++ Star-Realms/src/model/Store.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,7 +26,10 @@ import view.GraphicStore;
  *
  */
 public class Store implements GraphicStore{
-
+	
+	/**
+	 * The last 5 cards of this array is the 5 cards of six cards that player can see.
+	 */
 	private final ArrayList<GameCard> cards;
 	private final GameCard explorer;
 
@@ -35,10 +39,6 @@ public class Store implements GraphicStore{
 	 * and a card Explorer.
 	 * </p>
 	 * 
-	 * <p>
-	 * Once the store has been created, we can request 6 cards from this store.
-	 * </p>
-	 * 
 	 * @param storeCards A qualified card deck, which means
 	 * @param explorer   A ship object who's name is Explorer
 	 */
@@ -46,49 +46,37 @@ public class Store implements GraphicStore{
 		cards = new ArrayList<GameCard>(storeCards);
 		this.explorer = Objects.requireNonNull(explorer);
 	}
-
+	
 	/**
-	 * This method allows us to visualize all the visible cards
+	 * Get the card specific by number.
+	 * 
+	 * @param i the number of card in the view of player. begin with 1.
+	 * @return the card.
 	 */
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		int i = 1;
-		for (GameCard c : peek()) {
-			sb.append("_____________________________\n");
-			sb.append("[" + i + "]" + c.toString() + '\n');
-			i++;
-		}
-		return sb.toString();
-	}
-
-	/**
-	 * <p>
-	 * This method return six available cards, the last one will always be the
-	 * Explorer.
-	 * </p>
-	 * 
-	 * <p>
-	 * This method selects last five card in "cardsToSell" as cards available.
-	 * </p>
-	 * 
-	 * 
-	 * @return {@code ArrayList} contains 6 GameCard
-	 */
-	public ArrayList<GameCard> peek() {
-		ArrayList<GameCard> cardsVisible = new ArrayList<GameCard>();
-		cardsVisible.add(explorer);
-		return cardsVisible;
-	}
-
 	public GameCard get(int i) {
 		int last = cards.size();
 		return cards.get(last-i);
 	}
 	
+	/**
+	 * Remove the card specific by number.
+	 * @param i the number of card in the view of player. begin with 1.
+	 * @return the card removed.
+	 */
 	public GameCard remove(int i) {
 		int last = cards.size();
 		return cards.remove(last-i);
+	}
+
+	@Override
+	public List<GameCard> cards() {
+		ArrayList<GameCard> cards = new ArrayList<GameCard>();
+		for(int i =1; i<=5;i++) {
+			cards.add(get(i));
+		}
+		cards.add(explorer);
+		return Collections.unmodifiableList(cards);
+		
 	}
 
 
