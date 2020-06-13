7e35c9750122abc3f1ef7c65550bf3bb23428b8d
Wenke-D <65447561+Wenke-D@users.noreply.github.com>
2020-06-13 15:02:27 +0200

    AI implement extra flow

--- Star-Realms/src/model/player/AiPlayer.java
+++ Star-Realms/src/model/player/AiPlayer.java
@@ -4,10 +4,20 @@ import java.util.List;
 
 import model.card.Card;
 
+/**
+ * A AiPlayer play the game without receiving input.
+ * @author Matth
+ *
+ */
 public class AiPlayer extends AbstractPlayer {
 
 	public AiPlayer(int tradePoint, int combatPoint, int authorityPoint, List<Card> list) {
 		super(tradePoint, combatPoint, authorityPoint, list);
+	}
+
+	@Override
+	public List<String> needExtraInput(String place, int cardIndex, String abilityType) {
+		throw new UnsupportedOperationException("Ai player shouldn't be test if need extra info");
 	}
 
 
