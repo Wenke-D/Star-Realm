6b03329e4838ed8024f9788c3d4c387ba8a4dd7d
franncisca <13262275365@163.com>
2020-07-08 20:47:48 +0200

    update PlayerNonAttacks state But still have problem with method Player.active() Don't know how to get type and extraInfos

--- Star-Realms/src/model/PlayerNonAttack.java
+++ Star-Realms/src/model/PlayerNonAttack.java
@@ -1,54 +1,69 @@
 package model;
 
-import projet.model.Player;
+import model.player.Player;
 
 public class PlayerNonAttack implements State {
 	private Player currentPlayer;
 	private Player opponent;
 	private Store store;
-	private Data data;
+	private DataKnight data;
 	
-	public PlayerNonAttack(Player currentPlayer, Player opponent, Store store, Data data) {
+	public PlayerNonAttack(Player currentPlayer, Player opponent, Store store, DataKnight data) {
 		super();
 		this.currentPlayer = currentPlayer;
 		this.opponent = opponent;
 		this.store = store;
 		this.data = data;
 	}
-
+	/**
+	 * play this card and active the basic functions
+	 */
 	@Override
 	public void use(int i) {
-		currentPlayer.use();
+		currentPlayer.put(i);
+		currentPlayer.active(i, "basic", opponent, store, extraInfos);
 		
 	}
 
+	/**
+	 * active special functions (allay or scrape)
+	 */
 	@Override
 	public void active(int i) {
-		// TODO Auto-generated method stub
+		currentPlayer.active(i, type, opponent, store, extraInfos);
 		
 	}
 
 	@Override
 	public void buy(int i) {
-		// TODO Auto-generated method stub
-		
+		int price = store.get(i).getCost();
+		if(currentPlayer.canAfford(price)) {
+			currentPlayer.pay(price);
+		}
 	}
 
 	@Override
 	public void attackPlayer() {
-		// TODO Auto-generated method stub
+		currentPlayer.attack(opponent);
 		
 	}
 
 	@Override
 	public void attackBase(int i) {
-		// TODO Auto-generated method stub
-		
+		int damage = currentPlayer.getCombat();
+		if(opponent.baseIsDestory(i, damage)) {
+			opponent.destoryCard(i);
+		}	
 	}
 
 	@Override
 	public void end() {
-		// TODO Auto-generated method stub
+		currentPlayer.endTurn();
+		currentPlayer.drawCard(5);
+
+		// ResetState()
+		opponent.beginTurn(currentPlayer, store);
+		//switchPlayer();
 		
 	}
 	
